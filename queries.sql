e30e6f600e378456b3e631e64a3b88ad72321561
edubew <edubew98@gmail.com>
2023-05-04 00:35:50 +0300

    Update README file

--- queries.sql
+++ queries.sql
@@ -1,6 +1,5 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
 SELECT name FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE EXTRACT(YEAR FROM date_of_birth) BETWEEN 2016 AND 2019;
 SELECT name FROM animals WHERE neutered = TRUE AND escape_attempts <3;
