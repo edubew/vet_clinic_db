846af26a146ba340f27240983fc75fd2571916ea
edubew <edubew98@gmail.com>
2023-05-05 00:24:00 +0300

    Write queries to update animals table with transactions

--- queries.sql
+++ queries.sql
@@ -7,4 +7,29 @@ SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT name, neutered FROM animals WHERE neutered = TRUE;
 SELECT name FROM animals WHERE NOT (name = 'Gabumon');
- SELECT name, weight_kg FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+SELECT name, weight_kg FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- Transaction1 starts
+BEGIN;
+
+UPDATE animals
+SET species = 'unspecified';
+
+SELECT name FROM animals WHERE species = 'unspecified';
+
+ROLLBACK;
+-- Transaction1 ends
+
+-- Transaction 2 starts
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+COMMIT;
+SELECT * FROM animals;
+-- Transaction 2 ends
